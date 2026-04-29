91e9331110fee2dc78542017de6e6f2c2a0b5c5a
bituan <113036994+bituann@users.noreply.github.com>
2026-04-29 17:19:27 +0100

    feat(auth): add unauthorized route

---
+++ app/api/auth/unauthorized/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const response = NextResponse.redirect(new URL('/login', request.url))
+  response.cookies.delete('access_token')
+  response.cookies.delete('refresh_token')
+  response.cookies.delete('role')
+  return response
+}
